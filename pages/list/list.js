24be0554dfaac306f2a718a25bdd2148fc8de844
iwen <1044803551@qq.com>
2018-08-14 03:59:18 +0800

    8/14

---
+++ pages/list/list.js
@@ -0,0 +1,90 @@
+// pages/list/list.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        lists: [{
+                id: 1,
+                title: "aaaaaa",
+                img: "../../images/1.png",
+                cTime: "2018-08-14 01:24"
+            },
+            {
+                id: 2,
+                title: "bbbbbb",
+                img: "../../images/2.png",
+                cTime: "2018-08-14 01:24"
+            },
+            {
+                id: 3,
+                title: "ccccc",
+                img: "../../images/3.png",
+                cTime: "2018-08-14 01:24"
+            },
+            {
+                id: 4,
+                title: "ddddd",
+                img: "../../images/4.png",
+                cTime: "2018-08-14 01:24"
+            }
+        ]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
+})
